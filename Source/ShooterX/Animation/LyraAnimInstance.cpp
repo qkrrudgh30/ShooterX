61098bbe2cd535455deb75eec74ae4ebe44a5c05
qkrrudgh30 <qkrrudgh30@naver.com>
2025-09-11 22:05:33 +0900

    AnimInstance

---
+++ Source/ShooterX/Animation/LyraAnimInstance.cpp
@@ -0,0 +1,8 @@
+// LyraAnimInstance.cpp
+
+#include "LyraAnimInstance.h"
+
+ULyraAnimInstance::ULyraAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
---
+++ Source/ShooterX/Animation/LyraAnimInstance.h
@@ -0,0 +1,20 @@
+// LyraAnimInstance.h
+
+#pragma once
+
+#include "Animation/AnimInstance.h"
+#include "LyraAnimInstance.generated.h"
+
+UCLASS()
+class ULyraAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	ULyraAnimInstance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	// 해당 속성값은 라이라 프로젝트의 애니메이션 블루프린트에 사용되는 값이므로 정의 필수.
+	UPROPERTY(BlueprintReadOnly, Category="Character State Data")
+	float GroundDistance = -1.f;
+
+};
